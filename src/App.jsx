85c8dc44f77a7ebe2e33d35a7572ee0d9954daab
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-31 14:57:41 +0900

    background

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import NumberGuessGame from "./components/NumberGuessGame";
 
 const App = () => {
   return (
-    <div className="bg-yellow-100 min-h-screen flex justify-center items-center">
+    <div className="bg-gradient-to-br from-yellow-200 to-pink-300 min-h-screen flex justify-center items-center">
       <NumberGuessGame />
     </div>
   );
